cf00040213496e0889617e96ef20a9508b5866ae
chriswillphoto <christophernekich@gmail.com>
2018-10-12 07:10:38 +1100

    attempting to avoid regex

--- javascript/wordy/wordy.js
+++ javascript/wordy/wordy.js
@@ -1,10 +1,21 @@
 export class WordProblem {
-  constructor(question){
+  constructor(question) {
     this.question = question;
   }
 
-  answer(){
-    
+  answer() {
+    let index1;
+    let index2;
+    const questionString = this.question
+
+    for (let i = 0; i < questionString.length; i++) {
+      if (parseInt(questionString[i]) && !index1) {
+        index1 = i;
+      } else if (parseInt(questionString[i])) {
+        index2 = i;
+      }
+    }
+
+    console.log(index1, index2)
   }
 }
-
