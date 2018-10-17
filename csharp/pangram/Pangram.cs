c68b3088954a0c3889ec9b68ab1d7c97087ad984
chriswillphoto <christophernekich@gmail.com>
2018-10-17 21:38:45 +1100

    started pangram

---
+++ csharp/pangram/Pangram.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+public static class Pangram
+{
+    public static bool IsPangram(string input)
+    {
+        char[] alpha = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+        int alphaCounter = 0;
+
+        for (int i = 0; i < input.Length; i++)
+        {
+            for (int j = 0; j < 26; j++)
+            {
+                
+            }
+        }
+
+    }
+}
