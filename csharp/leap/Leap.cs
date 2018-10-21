88972b936d494c831d5e49b6cbf7e96960e48a02
chriswillphoto <christophernekich@gmail.com>
2018-10-21 20:21:46 +1100

    refactor

--- csharp/leap/Leap.cs
+++ csharp/leap/Leap.cs
@@ -4,52 +4,17 @@ public static class Leap
 {
   public static bool IsLeapYear(int year)
   {
-    if (year % 4 == 0)
-    {
-      if (DivisibleBy100(year))
-      {
-        if (DivisibleBy400(year))
-        {
-          return true;
-        }
-        else
-        {
-          return false;
-        }
-      }
-      else
-      {
-        return true;
-      }
-
-    }
-    else
-    {
-      return false;
-    }
-  }
-
-  public static bool DivisibleBy100(int year)
-  {
-    if (year % 100 == 0)
+    if(year % 4 == 0 && year % 100 == 0 && year % 400 == 0)
     {
       return true;
     }
-    else
-    {
-      return false;
-    }
-  }
 
-  public static bool DivisibleBy400(int year)
-  {
-    if (year % 400 == 0)
+    if(year % 4 == 0 && year % 100 != 0)
     {
       return true;
     }
-    else
-    {
-      return false;
-    }
+
+    return false;
+
   }
 }
