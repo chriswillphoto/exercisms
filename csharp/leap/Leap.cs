279e0f2ea549da397fda7406a4039a0a5e61cd81
chriswillphoto <christophernekich@gmail.com>
2018-10-27 15:31:26 +1100

    refactor

--- csharp/leap/Leap.cs
+++ csharp/leap/Leap.cs
@@ -4,12 +4,7 @@ public static class Leap
 {
   public static bool IsLeapYear(int year)
   {
-    if(year % 4 == 0 && year % 100 == 0 && year % 400 == 0)
-    {
-      return true;
-    }
-
-    if(year % 4 == 0 && year % 100 != 0)
+    if( (year % 4 == 0 && year % 100 == 0 && year % 400 == 0) || (year % 4 == 0 && year % 100 != 0) )
     {
       return true;
     }
